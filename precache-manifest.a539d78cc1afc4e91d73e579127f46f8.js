1d3ab88285bb6f0129de502e2862d8b8a997fb33
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2020-03-09 22:21:12 -0500

    Updates

---
+++ precache-manifest.a539d78cc1afc4e91d73e579127f46f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a33ff82e41f40a2ae6453f0a13cda87",
+    "url": "/riskystrats/index.html"
+  },
+  {
+    "revision": "9f47d37461bf3cc946df",
+    "url": "/riskystrats/static/css/main.f5edc999.chunk.css"
+  },
+  {
+    "revision": "b239a90aa9fc02d565f3",
+    "url": "/riskystrats/static/js/2.89cddd11.chunk.js"
+  },
+  {
+    "revision": "1201c8656ff5734f581290e1df7d9b22",
+    "url": "/riskystrats/static/js/2.89cddd11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f47d37461bf3cc946df",
+    "url": "/riskystrats/static/js/main.884d64a3.chunk.js"
+  },
+  {
+    "revision": "0c1bc4b0e501eb7e8af4",
+    "url": "/riskystrats/static/js/runtime-main.0089774d.js"
+  }
+]);
